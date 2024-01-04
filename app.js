675e105f441e8dc5984995ea31b7abce321c15a9
Ramo <ra58ad@gmail.com>
2024-01-04 22:07:17 +0300

    some js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!")
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form")
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault()
+        const username = document.querySelector("input").value
+        const response = await fetch(`https://api.github.com/users/${username}`)
+        const data = await response.json()
+        console.log(data)
+    })
+})
